1de336f875112b349a0b626639a175d963325b4f
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-05 15:08:06 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,8 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT pr.title, pl.amount FROM projects pr 
 INNER JOIN pleges pl 
-ON pr.id = pl.project_id ASC;"
+ON pr.id = pl.project_id 
+ORDER BY pr.title ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
